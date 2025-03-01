001a6d0be6e29d2d3d31f74539ec0858c53ba933
nusrathumaira12 <nusrathum31@gmail.com>
2025-03-02 00:34:11 +0600

    added new section

--- script/events.js
+++ script/events.js
@@ -1,1 +1,25 @@
-alert();
+const colors = ["yellow","pink","Blue" , "red"]
+let index = 0;
+
+document.getElementById('theme').addEventListener("click", function(event){
+event.preventDefault();
+document.body.style.backgroundColor = colors[index];
+index = (index + 1) % colors.length;
+})
+
+// document.querySelectorAll(".btn")
+const btns = document.getElementsByClassName("btn")
+//   console.log("hello")
+for(const btn of btns){
+   btn.addEventListener("click", function(){
+    alert()
+   })
+}
+    
+
+   
+
+
+// function btn(){
+//    alert()
+// }
