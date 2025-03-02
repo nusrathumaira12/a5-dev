bc9986be352f520f8a3b8be067a29e6ab50a8b6a
nusrathumaira12 <nusrathum31@gmail.com>
2025-03-02 22:31:26 +0600

    added new html file

--- script/activities.js
+++ script/activities.js
@@ -1,14 +1,34 @@
-document.getElementById("show").style.display = "none"
+const logContainer = document.getElementById("logContainer");
+logContainer.style.display = "none"
 
-const btns = document.getElementsByClassName("btn")
-//   console.log("hello")
-for(const btn of btns){
-   btn.addEventListener("click", function(){
-const div = document.getElementById("show");
-div.style.display= "block"
+const btnns = document.querySelectorAll(".btn")
 
-
-   })
+// const newLog = document.createElement("p")
+const titles = document.getElementsByClassName("title")
+// console.log(titles)
+for(const title of titles){
+    // console.log(title.innerText)
+    const newT = title.innerText
+    console.log(newT)
 }
+
+for(btn of btns){
+    btn.addEventListener("click", function(){
+logContainer.style.display = "block"
+
+const timeClicked = new Date().toLocaleTimeString()
+const newLog = document.createElement("p")
+newLog.classList.add("text-gray-400", "font-bold", "text-sm", "p-2");
+      newLog.innerHTML = `
+      you have completed the task "<span class = " text-gray-400 font-bold text-sm"> ${titles.innerText}</span>" <span class="text-gray-600"> ${timeClicked}PM</span>;
+      
+      `
+      logContainer.appendChild(newLog);
+    })
+}
+
+document.getElementById("clear").addEventListener("click", function(){
+    document.getElementById("logContainer").style.display = "none"
+})
 
 
--- script/events.js
+++ script/events.js
@@ -1,49 +1,74 @@
-const colors = ["yellow","pink","Blue" , "red"]
+const colors = ["yellow", "pink", "Blue", "red"]
 let index = 0;
 
-document.getElementById('theme').addEventListener("click", function(event){
-event.preventDefault();
-document.body.style.backgroundColor = colors[index];
-index = (index + 1) % colors.length;
-})
+document.getElementById('theme').addEventListener("click", function (event) {
+   event.preventDefault();
+   document.body.style.backgroundColor = colors[index];
+   index = (index + 1) % colors.length;
+});
 
 // document.querySelectorAll(".btn")
-const btns = document.getElementsByClassName("btn")
+const btns = document.querySelectorAll(".btn")
+// const logContainer = document.getElementById("logContainer")
+let clickedCount = 0;
+const totalButtons = btns.length;
+console.log(totalButtons)
 //   console.log("hello")
-for(const btn of btns){
-   btn.addEventListener("click", function(){
-    alert("Board updated Successfully");
-
-    this.disabled = true;
-
- const notAdd = document.getElementById("low")
- const add = document.getElementById("up")
- 
-//  console.log(notAdd)
-//  console.log(typeof notAdd.innerText)
-const convertedNotAdd = parseFloat(notAdd.innerText)
-const convertedAdd = parseFloat(add.innerText)
-
-// console.log(typeof convertedNotAdd)
-let result = convertedNotAdd - 1;
-let result2 = convertedAdd + 1;
-
-console.log(result)
-document.getElementById("low").innerText = result;
-document.getElementById("up").innerText = result2;
-// console.log(typeof add.innerText)
-// console.log(typeof convertedAdd);
-console.log(result2)
+for (const btn of btns){
+   btn.addEventListener("click", function () {
+      alert("Board updated Successfully");
 
 
+      this.disabled = true;
+      clickedCount++;
 
-   })
+      // const taskTitle = titles[index].innerText;
+      // const timeClicked = new Date().toLocaleTimeString()
+      // const newLog = document.createElement("p")
+
+
+      // newLog.classList.add("text-gray-400", "font-bold", "text-sm", "p-2");
+      // newLog.innerHTML = `
+      // you have completed "<span class = "text-gray-600"> ${taskTitle}</span>" <span class="text-gray-600"> ${timeClicked}PM</span>;
+      
+      // `
+      // logContainer.appendChild(newLog);
+
+
+      if (clickedCount === totalButtons) {
+         setTimeout(() => {
+            alert("congrates!!!you have completed all the current task");
+         }, 500);
+      }
+
+         // const titles = document.querySelectorAll(".title");
+         // titles.forEach(title => console.log(title.innerText)
+         // );
+         // const timeClicked = new Date().toLocaleTimeString();
+         
+        
+     
+
+      const notAdd = document.getElementById("low")
+      const add = document.getElementById("up")
+
+      //  console.log(notAdd)
+      //  console.log(typeof notAdd.innerText)
+      const convertedNotAdd = parseFloat(notAdd.innerText)
+      const convertedAdd = parseFloat(add.innerText)
+
+      // console.log(typeof convertedNotAdd)
+      let result = convertedNotAdd - 1;
+      let result2 = convertedAdd + 1;
+
+
+      console.log(result)
+      document.getElementById("low").innerText = result;
+      document.getElementById("up").innerText = result2;
+
+      // console.log(typeof add.innerText)
+      // console.log(typeof convertedAdd);
+      console.log(result2)
+   });
 }
-    
 
-   
-
-
-// function btn(){
-//    alert()
-// }
